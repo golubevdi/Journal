5ef7bebce9d602bc0a929ca8d965ab856739397f
golubevdi <golubevdi@masterscada.ru>
2021-10-12 18:22:26 +0300

    Comm

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -9,10 +9,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 @DisplayName("Тесты архивного журнала")
 @ExtendWith(ScreenshotExt.class)
 public class MainTest {
+    //Параметр для проверки теста 1
+    final String parameter_1_input = "-квит_1";
+    final String comment_input = "квитировано";
+
+    //Параметр для проверки теста 2
+    final String parameter_1_input_1 = "-ack_2";
+    final String comment_input_1 = "acked";
+
+    //Параметр для проверки теста 3
     final String color_act = "background-color: aqua;";
     final String color_deact = "background-color: gold;";
     final String color_ack = "background-color: crimson;";
 
+    //Параметр для проверки теста 4
+    final String message_1_input = "сообщение";
+    final String comment_1_input = "комментарий";
+    final String message_2_input = "message";
+    final String comment_2_input = "comment";
 
     ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
@@ -27,14 +41,10 @@ public class MainTest {
         //Создаем новый объект chromedriver
         driver.set(new ChromeDriver());
         //Полноэкранный режим driver.manage().window().maximize();
-    }
-/*
+    }/*
     @Test
     @DisplayName("Проверка отображения пользовательского параметра тревоги в журнале (русские символы)")
     public void test1() throws InterruptedException {
-        //Параметр для проверки
-        final String parameter_1_input = "-квит_1";
-        final String comment_input = "квитировано";
 
         archiveJournal1 aJournal1 = new archiveJournal1(driver.get());
 
@@ -48,17 +58,14 @@ public class MainTest {
     @Test
     @DisplayName("Проверка отображения пользовательского параметра тревоги в журнале (английские символы)")
     public void test2() throws InterruptedException {
-        //Параметр для проверки
-        final String parameter_1_input = "-ack_2";
-        final String comment_input = "acked";
 
         archiveJournal1 aJournal1 = new archiveJournal1(driver.get());
 
         aJournal1.StartTest();
-        aJournal1.ActivateMessage(parameter_1_input);
-        aJournal1.AckedMessage(parameter_1_input, comment_input);
-        aJournal1.RefreshPage(parameter_1_input);
-        aJournal1.fileCSV(parameter_1_input, comment_input);
+        aJournal1.ActivateMessage(parameter_1_input_1);
+        aJournal1.AckedMessage(parameter_1_input_1, comment_input_1);
+        aJournal1.RefreshPage(parameter_1_input_1);
+        aJournal1.fileCSV(parameter_1_input_1, comment_input_1);
         aJournal1.deleteFile();
     }
     @Test
@@ -66,6 +73,7 @@ public class MainTest {
     public void test3() throws InterruptedException {
 
         archiveJournal2 aJournal2 = new archiveJournal2(driver.get());
+
         aJournal2.StartTest();
         aJournal2.preparation();
         aJournal2.compareColor(color_act,color_deact,color_ack);
@@ -73,8 +81,10 @@ public class MainTest {
     @Test
     @DisplayName("Что-то там")
     public void test4() throws InterruptedException {
+
         archiveJournal3 aJournal3 = new archiveJournal3(driver.get());
+
         aJournal3.StartTest();
-        aJournal3.preparation1();
+        aJournal3.preparation1(message_1_input,comment_1_input,message_2_input,comment_2_input);
     }
 }
--- src/test/java/archiveJournal3.java
+++ src/test/java/archiveJournal3.java
@@ -15,6 +15,10 @@ public class archiveJournal3 extends MainTest{
     public static final By test3Button = By.id("104312");
     public static final By activateButton1 = By.id("103114");
     public static final By activateButton2 = By.id("103575");
+    public static final By textMessage1 = By.id("103484");
+    public static final By textComment1 = By.id("104002");
+    public static final By textMessage2 = By.id("104083");
+    public static final By textComment2 = By.id("104100");
 
     public archiveJournal3(WebDriver driver) {
 
@@ -27,7 +31,7 @@ public class archiveJournal3 extends MainTest{
         this.driver.get(Base_URL);
     }
     @Step("Проверка отображения в журнале пользовательского параметра тревоги")
-    public void preparation1() throws InterruptedException{
+    public void preparation1(String message_1_input, String comment_1_input, String message_2_input, String comment_2_input) throws InterruptedException{
         JavascriptExecutor jse = (JavascriptExecutor) driver;
         //Ожидание (загрузка страницы, элементов)
         Thread.sleep(2000);
@@ -41,7 +45,7 @@ public class archiveJournal3 extends MainTest{
                 jse.executeScript
                         ("return document.querySelector(\"#\\\\31 03114\").shadowRoot.querySelector(\"div[class]\")");
         String aClass1 = search_button_act1.getAttribute("class");
-        System.out.println(aClass1);
+        System.out.println("Состояние кнопки Активность Тревоги 1 до начала теста: " + aClass1);
         if (aClass1.equals("tbmain button active")) {
             driver.findElement(activateButton1).click();
         }
@@ -52,10 +56,52 @@ public class archiveJournal3 extends MainTest{
                 jse.executeScript
                         ("return document.querySelector(\"#\\\\31 03575\").shadowRoot.querySelector(\"div[class]\")");
         String aClass2 = search_button_act2.getAttribute("class");
-        System.out.println(aClass2);
+        System.out.println("Состояние кнопки Активность Тревоги 2 до начала теста: " + aClass2);
         if (aClass2.equals("tbmain button active")) {
             driver.findElement(activateButton2).click();
         }
+        //Увеличиваем значение инкремента Тревоги 1
+        WebElement search_button_inc_up = (WebElement)
+                jse.executeScript
+                        ("return document.querySelector(\"#\\\\31 03434\").shadowRoot.querySelector(\"#incr\")");
+        search_button_inc_up.click();
+        Thread.sleep(2000);
+        WebElement text_inc = (WebElement)
+                jse.executeScript
+                        ("return document.querySelector(\"#\\\\31 08016\").shadowRoot.querySelector(\"div\")");
+        String text_inc_output = text_inc.getText();
+        //вывод значения инкремента Тревоги 1 в консоль
+        System.out.println("приоритет тревоги 1: " + text_inc_output);
+        //Увеличиваем значение инкремента Тревоги 1
+        WebElement search_button_inc_up_2 = (WebElement)
+                jse.executeScript
+                        ("return document.querySelector(\"#\\\\31 04066\").shadowRoot.querySelector(\"#incr\")");
+        search_button_inc_up_2.click();
+        search_button_inc_up_2.click();
+        Thread.sleep(2000);
+        WebElement text_inc_2 = (WebElement)
+                jse.executeScript
+                        ("return document.querySelector(\"#\\\\31 08031\").shadowRoot.querySelector(\"div\")");
+        String text_inc2 = text_inc_2.getText();
+        //вывод значения инкремента Тревоги 2 в консоль
+        System.out.println("приоритет тревоги 2: " + text_inc2);
+        Thread.sleep(2000);
+
+        //Поиск элемента для задания сообщения Тревоги 1 и задание ему значения "сообщение"
+        driver.findElement(textMessage1).sendKeys(message_1_input);
+        //Поиск элемента для задания комментария Тревоги 1 и задание ему значения "комментарий"
+        driver.findElement(textComment1).sendKeys(comment_1_input);
+        //Поиск элемента для задания сообщения Тревоги 1 и задание ему значения "сообщение"
+        driver.findElement(textMessage2).sendKeys(message_2_input);
+        //Поиск элемента для задания комментария Тревоги 1 и задание ему значения "комментарий"
+        driver.findElement(textComment2).sendKeys(comment_2_input);
+
+        //Поиск кнопки "Активность" Тревоги 1 и клик по ней (кнопка с фиксацией, положение вкл.)
+        driver.findElement(activateButton1).click();
+        //Поиск кнопки "Активность" Тревоги 2 и клик по ней (кнопка с фиксацией, положение вкл.)
+        driver.findElement(activateButton2).click();
+
+        Thread.sleep(7000);
     }
 }
 
